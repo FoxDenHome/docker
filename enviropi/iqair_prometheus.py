bf52838a6836800c75b8a7e4c4e7b9080508d291
Doridian <git@doridian.net>
2021-08-13 15:26:23 -0700

    provide timestamps with metrics

--- enviropi/iqair_prometheus.py
+++ enviropi/iqair_prometheus.py
@@ -1,43 +1,66 @@
 #!/usr/bin/python3 -u
 
+from datetime import datetime
 from urllib.request import urlopen
 from time import sleep
 from json import load
-from prometheus_client import Gauge, start_http_server
+from prometheus_client import CollectorRegistry, REGISTRY, start_http_server
+from prometheus_client.core import GaugeMetricFamily
 
 URL = "https://website-api.airvisual.com/v1/stations/4d785ca890fb488c3bee"
 PARAMS = "units.temperature=celsius&units.distance=km&units.pressure=millibar&AQI=US&language=en-US"
 
 POLLUTANT_MAP = {
-    'pm1':  1.0,
-    'pm25': 2.5,
-    'pm10': 10,
+    'pm1':  '1.0',
+    'pm25': '2.5',
+    'pm10': '10',
 }
 
-gauge_air_ug_per_m3 = Gauge('air_ug_per_m3', 'PM in ug/m3', ['size', 'environment'])
-gauge_aqi = Gauge('aqi', 'AQI (US)')
-gauge_pressure = Gauge('pressure', 'Pressure in millibar')
-gauge_humidity = Gauge('humidity', 'Relative humidity in %')
-gauge_temperature = Gauge('temperature', 'Temperature in Celsius')
+REGISTRY = CollectorRegistry()
 
-gauge_wind_speed = Gauge('wind_speed', 'Wind speed in km/h')
-gauge_wind_direction = Gauge('wind_direction', 'Wind direction in degrees')
+gauge_air_ug_per_m3 = GaugeMetricFamily('air_ug_per_m3', 'PM in ug/m3', labels=['size', 'environment'])
+gauge_aqi = GaugeMetricFamily('aqi', 'AQI (US)')
+gauge_pressure = GaugeMetricFamily('pressure', 'Pressure in millibar')
+gauge_humidity = GaugeMetricFamily('humidity', 'Relative humidity in %')
+gauge_temperature = GaugeMetricFamily('temperature', 'Temperature in Celsius')
+gauge_wind_speed = GaugeMetricFamily('wind_speed', 'Wind speed in km/h')
+gauge_wind_direction = GaugeMetricFamily('wind_direction', 'Wind direction in degrees')
+
+class FakeCollector():
+    def __init__(self):
+        pass
+
+    def collect(self):
+        return [
+            gauge_air_ug_per_m3,
+            gauge_aqi,
+            gauge_pressure,
+            gauge_humidity,
+            gauge_temperature,
+            gauge_wind_speed,
+            gauge_wind_direction,
+        ]
+
+REGISTRY.register(FakeCollector())
 
 def load_station():
     data = load(urlopen("%s?%s" % (URL, PARAMS)))['current']
 
-    gauge_aqi.set(data['aqi'])
-    gauge_pressure.set(data['pressure'])
-    gauge_humidity.set(data['humidity'])
-    gauge_temperature.set(data['temperature'])
+    dt = datetime.strptime(data['ts'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    timestamp = dt.timestamp()
 
-    gauge_wind_speed.set(data['wind']['speed'])
-    gauge_wind_direction.set(data['wind']['direction'])
+    gauge_aqi.add_metric([], data['aqi'], timestamp)
+    gauge_pressure.add_metric([], data['pressure'], timestamp)
+    gauge_humidity.add_metric([], data['humidity'], timestamp)
+    gauge_temperature.add_metric([],data['temperature'], timestamp)
+
+    gauge_wind_speed.add_metric([], data['wind']['speed'], timestamp)
+    gauge_wind_direction.add_metric([], data['wind']['direction'], timestamp)
 
     for pollutant in data['pollutants']:
-        gauge_air_ug_per_m3.labels(size=POLLUTANT_MAP[pollutant['pollutantName']], environment='outside').set(pollutant['concentration'])
+        gauge_air_ug_per_m3.add_metric([POLLUTANT_MAP[pollutant['pollutantName']], 'outside'], pollutant['concentration'], timestamp)
 
-start_http_server(8003)
+start_http_server(8003, registry=REGISTRY)
 
 while True:
     print('Polling IQAir...')
