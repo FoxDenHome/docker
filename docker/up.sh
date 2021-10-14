6b804d3d2f90a15f1d30f6af71e646028533e903
Mark Dietzer <git@doridian.net>
2021-10-14 14:40:49 -0700

    load indiv

--- docker/up.sh
+++ docker/up.sh
@@ -3,14 +3,14 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-CONFIGS=""
-
 load_role() {
+    CONFIGS=""
     DIR="$1"
     for file in `find "$DIR" -type f -name '*.yml'`
     do
         CONFIGS="$CONFIGS -f $file"
     done
+    docker-compose $CONFIGS -f networks.yml up -d
 }
 
 HN="$(hostname)"
@@ -18,5 +18,3 @@ for role in `cat "${HN}.roles"`
 do
     load_role "$role"
 done
-
-docker-compose $CONFIGS up -d
