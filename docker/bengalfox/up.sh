f621b7156079e25ffc61cc42b5461fa1e7dac23c
Mark Dietzer <git@doridian.net>
2021-10-14 12:32:12 -0700

    add e621dumper

--- docker/bengalfox/up.sh
+++ docker/bengalfox/up.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose -f networks.yml -f samba.yml -f foxcaves.yml -f plex.yml -f dldr.yml -f syncthing.yml up -d
+docker-compose -f networks.yml -f samba.yml -f foxcaves.yml -f plex.yml -f dldr.yml -f syncthing.yml -f e621dumper.yml up -d
