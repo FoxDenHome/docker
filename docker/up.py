023727c2e71b8b7e48386003f7adf93d6676ce28
Mark Dietzer <git@doridian.net>
2021-10-15 09:53:01 -0700

    declare globalnets we need

--- docker/up.py
+++ docker/up.py
@@ -4,22 +4,103 @@ from os import chdir, listdir
 from os.path import dirname, abspath
 from socket import gethostname
 from subprocess import run
+from yaml import load as yaml_load, dump as yaml_dump
+from yaml.loader import SafeLoader
+from tempfile import NamedTemporaryFile
 
 chdir(dirname(abspath(__file__)))
+
+DOCKER_COMPOSE_VERSION = "2.4"
+
+def yaml_loadfile(file):
+    fh = open(file, "r")
+    data = yaml_load(fh, Loader=SafeLoader)
+    fh.close()
+    return data
+
+class ComposeProject():
+    def __init__(self, name):
+        self.used_networks = set()
+        self.provided_networks = set()
+        self.name = name
+        self.files = set()
+
+    def load_dir(self, dir):
+        for filename in listdir(dir):
+            if not filename.endswith(".yml"):
+                continue
+            file = f"{dir}/{filename}"
+            self.add_yaml(file)
+
+    def add_yaml(self, file):
+        if file in self.files:
+            return
+
+        data = yaml_loadfile(file)
+        if data["version"] != DOCKER_COMPOSE_VERSION:
+            raise Exception(f"Unsupported compose file version: {data['version']}")
+
+        if "services" in data:
+            for service in data["services"]:
+                self.add_service(service, data["services"][service])
+
+        if "networks" in data:
+            for network in data["networks"]:
+                self.provided_networks.add(network)
+
+        self.files.add(file)
+
+    def add_service(self, _, data):
+        if "networks" not in data:
+            return
+        for network in data["networks"]:
+            if network == "default":
+                continue
+            self.used_networks.add(network)
+
+    def get_missing_networks(self):
+        return self.used_networks - self.provided_networks
+
+    def deploy(self):
+        if self.get_missing_networks():
+            raise Exception(f"Missing network definitions for: {','.join(self.get_missing_networks())}")
+
+        compose_args = ["docker-compose", "-p", self.name]
+        for file in self.files:
+            compose_args.append("-f")
+            compose_args.append(file)
+        
+        run(compose_args + ["pull"])
+        run(compose_args + ["up", "-d", "--remove-orphans"])
+
+GLOBAL_NETWORKS = yaml_loadfile("networks.yml")["networks"]
 
 def load_role(role):
     if role == "":
         return
     print("Loading role", role)
-    compose_args = ["docker-compose", "-p", role]
-    for file in listdir(role):
-        if file.endswith(".yml"):
-            compose_args.append("-f")
-            compose_args.append(f"{role}/{file}")
-    compose_args += ["-f", "networks.yml"]
 
-    run(compose_args + ["pull"])
-    run(compose_args + ["up", "-d", "--remove-orphans"])    
+    project = ComposeProject(role)
+    project.load_dir(role)
+
+    temp_file = None
+    missing_networks = project.get_missing_networks()
+    if missing_networks:
+        used_global_nets = {
+            "version": DOCKER_COMPOSE_VERSION,
+            "networks": {
+                network: GLOBAL_NETWORKS[network]
+                for network in missing_networks
+            }
+        }
+        temp_file = NamedTemporaryFile(mode="w+", suffix=".yml")
+        yaml_dump(used_global_nets, temp_file)
+        temp_file.flush()
+        project.add_yaml(temp_file.name)
+
+    project.deploy()
+    if temp_file:
+        temp_file.close()
 
 ROLES_FILE = f"{gethostname().lower()}.roles"
 
