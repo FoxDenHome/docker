c82bbc2d36e34b8585748d339f32f76906e11fac
Doridian <git@doridian.net>
2021-11-07 14:33:40 -0800

    sed?

--- docker/monitoring/prometheus/preproc.sh
+++ docker/monitoring/prometheus/preproc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-envsubst < /etc/prometheus/prometheus.yml.tpl > /etc/prometheus/prometheus.yml
+sed "s~__HOMEASSISTANT_API_TOKEN__~$HOMEASSISTANT_API_TOKEN~" < /etc/prometheus/prometheus.yml.tpl > /etc/prometheus/prometheus.yml
 
 exec /bin/prometheus "$@"
