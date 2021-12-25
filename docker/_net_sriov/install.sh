66db151fabf9256281e255bef888c9eb9ad52921
Doridian <git@doridian.net>
2021-12-25 13:24:05 -0800

    a

--- docker/_net_sriov/install.sh
+++ docker/_net_sriov/install.sh
@@ -6,7 +6,7 @@ VERSION="v1.0.1"
 rm -f /usr/local/bin/docker-sriov-plugin /usr/local/bin/ibdev2netdev /etc/systemd/system/docker-sriov-plugin.service
 wget "https://raw.githubusercontent.com/Doridian/docker-sriov-plugin/$VERSION/ibdev2netdev" -O /usr/local/bin/ibdev2netdev
 wget "https://github.com/Doridian/docker-sriov-plugin/releases/download/$VERSION/docker-sriov-plugin" -O /usr/local/bin/docker-sriov-plugin
-wget "https://raw.githubusercontent.com/Doridian/docker-sriov-plugin/$VERSION/ibdev2netdev" -O /etc/systemd/system/docker-sriov-plugin.service
+wget "https://raw.githubusercontent.com/Doridian/docker-sriov-plugin/$VERSION/docker-sriov-plugin.service" -O /etc/systemd/system/docker-sriov-plugin.service
 chmod +x /usr/local/bin/docker-sriov-plugin /usr/local/bin/ibdev2netdev
 
 systemctl enable docker-sriov-plugin
