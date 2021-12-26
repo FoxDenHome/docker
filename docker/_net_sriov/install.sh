edd68a734cb113203bed7a7c5534155d808aaaed
Doridian <git@doridian.net>
2021-12-25 16:04:07 -0800

    update installer

--- docker/_net_sriov/install.sh
+++ docker/_net_sriov/install.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
-set -e
+set -ex
 
-VERSION="v1.1.0"
+VERSION="1.2.0"
+ARCH="amd64"
 
-rm -f /usr/local/bin/docker-sriov-plugin /usr/local/bin/ibdev2netdev /etc/systemd/system/docker-sriov-plugin.service
-wget "https://raw.githubusercontent.com/Doridian/docker-sriov-plugin/$VERSION/ibdev2netdev" -O /usr/local/bin/ibdev2netdev
-wget "https://github.com/Doridian/docker-sriov-plugin/releases/download/$VERSION/docker-sriov-plugin" -O /usr/local/bin/docker-sriov-plugin
-wget "https://raw.githubusercontent.com/Doridian/docker-sriov-plugin/$VERSION/docker-sriov-plugin.service" -O /etc/systemd/system/docker-sriov-plugin.service
-chmod +x /usr/local/bin/docker-sriov-plugin /usr/local/bin/ibdev2netdev
+DIR="$(mktemp -d)"
+cd "$DIR"
 
+wget "https://github.com/Doridian/docker-sriov-plugin/releases/download/$VERSION/docker-sriov-plugin-linux-$ARCH.tar.gz" -O artifact.tar.gz
+tar -xvf artifact.tar.gz
+
+mvf() {
+    DST="$1"
+    SRC="$(basename "$DST")"
+    rm -f "$DST"
+    mv "$SRC" "$DST"
+}
+
+mvf /usr/local/bin/docker-sriov-plugin
+mvf /usr/local/bin/ibdev2netde
+mvf /etc/systemd/system/docker-sriov-plugin.service
+
+cd /
+rm -rf "$DIR"
+
+systemctl daemon-reload
 systemctl enable docker-sriov-plugin
 systemctl restart docker-sriov-plugin
