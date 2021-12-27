7feb8f5541a710cc803138fd2c4a72722c74d882
Doridian <git@doridian.net>
2021-12-26 21:08:59 -0800

    nvcheck

--- docker/_nvidia/checker.py
+++ docker/_nvidia/checker.py
@@ -4,7 +4,7 @@ from subprocess import CalledProcessError, check_call, check_output
 
 def test_nvidia_ct(id: str):
     try:
-        res = check_output(["docker", "exec", id, "nvidia-smi", "-L"])
+        res = check_output(["docker", "exec", id, "nvidia-smi", "-L"]).decode("utf8")
         return "GPU 0" in res 
     except CalledProcessError:
         return False
@@ -16,5 +16,5 @@ def check_ct(id: str):
     if not test_nvidia_ct(id):
         check_call(["docker", "restart", id])
 
-for ct in check_output(["docker", "ps", "-f", "label=net.doridian.nvidia-check", "-f", "status=running", "--format", "{{.ID}}"]).strip().split("\n"):
+for ct in check_output(["docker", "ps", "-f", "label=net.doridian.nvidia-check", "-f", "status=running", "--format", "{{.ID}}"]).decode("utf8").strip().split("\n"):
     check_ct(ct)
