b3d92d072bf40b44ecf55f6d8b379d8f6a35deaa
Doridian <git@doridian.net>
2021-12-26 17:37:37 -0800

    check hash

--- docker/_net_sriov/install.sh
+++ docker/_net_sriov/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-VERSION="1.4.3"
+VERSION="1.4.4"
 ARCH="amd64"
 
 DIR="$(mktemp -d)"
@@ -12,23 +12,25 @@ purge() {
     rm -rf "$DIR"
 }
 purge_fail() {
+    echo "Error: $1"
     purge
     exit 1
 }
 
-wget "https://github.com/Doridian/docker-sriov-plugin/releases/download/$VERSION/docker-sriov-plugin-linux-$ARCH.tar.gz" -O artifact.tar.gz || purge_fail
-tar -xvf artifact.tar.gz || purge_fail
+wget "https://github.com/Doridian/docker-sriov-plugin/releases/download/$VERSION/docker-sriov-plugin-linux-$ARCH.tar.gz" -O artifact.tar.gz || purge_fail 'Downloading archive failed'
+wget "https://github.com/Doridian/docker-sriov-plugin/releases/download/$VERSION/docker-sriov-plugin-linux-$ARCH.tar.gz.sha256" -O artifact.tar.gz.sha256 || purge_fail 'Downloading checksum file failed'
+(echo "$(cat artifact.tar.gz.sha256) artifact.tar.gz" | sha256sum -c) || purge_fail 'Checksum wrong'
+tar -xvf artifact.tar.gz || purge_fail 'Could not unpack archive'
 
 mvf() {
     DST="$1"
     SRC="$(basename "$DST")"
     if [ ! -f "$SRC" ]
     then
-        echo "Could not find: $SRC"
-        purge_fail
+        purge_fail "Could not find: $SRC"
     fi
-    rm -f "$DST" || purge_fail
-    mv "$SRC" "$DST" || purge_fail
+    rm -f "$DST" || purge_fail "Could not remove: $DST"
+    mv "$SRC" "$DST" || purge_fail "Could not move $SRC to $DST"
 }
 
 mvf /usr/local/bin/docker-sriov-plugin
