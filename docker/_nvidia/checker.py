d643a4ccd1dc3e77400b6261c0ed80ce9b8c1cc4
Doridian <git@doridian.net>
2021-12-26 22:48:38 -0800

    only check healthy

--- docker/_nvidia/checker.py
+++ docker/_nvidia/checker.py
@@ -17,5 +17,5 @@ def check_ct(id: str):
         print(f"Check failed on {id}. Restarting!")
         check_call(["docker", "restart", id])
 
-for ct in check_output(["docker", "ps", "-f", "label=net.doridian.nvidia-check", "-f", "status=running" "-f", "health=healthy", "--format", "{{.ID}}"]).decode("utf8").strip().split("\n"):
+for ct in check_output(["docker", "ps", "-f", "label=net.doridian.nvidia-check", "-f", "status=running", "-f", "health=healthy", "--format", "{{.ID}}"]).decode("utf8").strip().split("\n"):
     check_ct(ct)
