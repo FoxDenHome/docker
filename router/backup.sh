26e72702aeeb3de8f149421201feccb16143d894
Doridian <git@doridian.net>
2022-01-05 13:57:41 -0800

    move steps

--- router/backup.sh
+++ router/backup.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 ssh router '/system/backup/save dont-encrypt=yes name=router.backup'
-scp router:/router.backup ./
-ssh router '/file/remove router.backup'
-
 ssh router '/export file=router.rsc show-sensitive'
-scp router:/router.rsc ./
-ssh router '/file/remove router.rsc'
 
+sleep 1
+
+scp router:/router.backup router:/router.rsc ./
 if [ ! -z "$1" ]
 then
     cp router.backup router.rsc "$1"
 fi
+
+sleep 1
+
+ssh router '/file/remove router.backup'
+ssh router '/file/remove router.rsc'
