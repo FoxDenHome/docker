366e96177ee9e3814c9d91584f53b95b607d5cf5
Mark Dietzer <git@doridian.net>
2022-01-28 22:34:36 -0800

    this should do it

--- _scripts/netfix.sh
+++ _scripts/netfix.sh
@@ -5,6 +5,11 @@ SELF="$0"
 
 if [ -z "$1" ]
 then
+    for container in docker ps --format '{{.Names}}'
+    do
+        "$SELF" "$container"
+    done
+
     docker events --filter 'event=start' --format '{{.Actor.Attributes.name}}' | 
     while read -r container
     do
@@ -21,9 +26,12 @@ then
     nsenter -n -t "$(docker inspect --format {{.State.Pid}} "$ID")" "$SELF" "$ID" "NSENTER"
 fi
 
+echo -n "Checking $container: "
+
 LAN_ROUTE="$(ip -4 route | grep '10\..*\.0\.0/16' | cut -d ' ' -f 1)"
 if [ -z "$LAN_ROUTE" ]
 then
+    echo 'No LAN route found'
     exit 0
 fi
 
@@ -31,3 +39,4 @@ LAN_GW="$(echo "$LAN_ROUTE" | sed s~.0/16~.1~)"
 
 ip -4 route del default
 ip -4 route add default via "$LAN_GW"
+echo 'Route adjusted'
