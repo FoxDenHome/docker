7c1f2a7e5e45d38d6d2b01442014419972bf7e79
Doridian <git@doridian.net>
2023-02-26 17:12:52 -0800

    update for gamescope wlroot

--- steam/steam/custom-entry.sh
+++ steam/steam/custom-entry.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -e
 
-steam
+export SEATD_VTBOUND=0
+unset DISPLAY
+
+eval "$(dbus-launch --auto-syntax)"
+
+pipewire &
+sleep 1
+
+gamescope -W 3840 -H 1600 -r 60 -e -- steam -tenfoot -steamos -pipewire-dmabuf
+
+echo "Session Running. Press [Return] to exit."
+read
+
+exit 0
