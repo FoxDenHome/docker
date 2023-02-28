c5aa2fb67ca12f87cc1fb25b7e4ed5d382faefbb
Doridian <git@doridian.net>
2023-02-27 20:11:25 -0800

    Gamescope once again

--- steam/steam/custom-entry.sh
+++ steam/steam/custom-entry.sh
@@ -9,7 +9,7 @@ eval "$(dbus-launch --auto-syntax)"
 pipewire &
 sleep 1
 
-gamescope -W 3840 -H 1600 -w 3840 -h 1600 -r 60 -e -- steam -tenfoot -steamos -pipewire-dmabuf
+gamescope -W 3840 -H 1600 -r 60 -e -- steam -tenfoot -steamos -pipewire-dmabuf
 
 echo "Session Running. Press [Return] to exit."
 read
