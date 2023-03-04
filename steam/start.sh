d95ba695de2e3b4790019502858390833d6610b1
Doridian <git@doridian.net>
2023-03-04 01:55:40 -0800

    autom8

---
+++ steam/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xeuo pipefail
+
+docker stop steam_steam-headless_1 || true
+
+export DISPLAY=':0'
+/usr/lib/xorg/Xorg vt8 -config /opt/docker/steam/xorg.conf -noreset -novtswitch -sharevts -dpi 96 +extension GLX +extension RANDR +extension RENDER +extension MIT-SHM "$DISPLAY" &
+sleep 1
+
+nvidia-smi -pm 1
+nvidia-settings -a 'GPUMemoryTransferRateOffsetAllPerformanceLevels=1000' -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUCurrentFanSpeed=100'
+
+sleep 1
+
+killall Xorg
+wait
+
+docker start steam_steam-headless_1 || true
---
+++ steam/stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xeuo pipefail
+
+docker stop steam_steam-headless_1 || true
+
+export DISPLAY=':0'
+/usr/lib/xorg/Xorg vt8 -config /opt/docker/steam/xorg.conf -noreset -novtswitch -sharevts -dpi 96 +extension GLX +extension RANDR +extension RENDER +extension MIT-SHM "$DISPLAY" &
+sleep 1
+
+nvidia-settings -a 'GPUMemoryTransferRateOffsetAllPerformanceLevels=0' -a '[gpu:0]/GPUFanControlState=0'
+nvidia-smi -pm 0
+
+sleep 1
+
+killall Xorg
+wait
