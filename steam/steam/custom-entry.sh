cb2854f766a2bf7097a702266a07e17cb603d8f3
Doridian <git@doridian.net>
2023-03-08 23:27:46 -0800

    vars

--- steam/steam/custom-entry.sh
+++ steam/steam/custom-entry.sh
@@ -2,4 +2,9 @@
 
 /usr/local/bin/modemgr --defaults
 
+if [ ! -z "$DEFAULT_SIZEW" ]
+then
+    /usr/local/bin/modemgr --switch -x $DEFAULT_SIZEW -y $DEFAULT_SIZEH -r $DEFAULT_REFRESH
+fi
+
 exec $KDE_START
