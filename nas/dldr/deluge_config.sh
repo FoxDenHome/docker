651dd3155baed839f3a7f8a50e859b59b9ae9400
Doridian <git@doridian.net>
2023-04-22 23:57:06 -0700

    Just modify deluge.sh

--- nas/dldr/deluge_config.sh
+++ nas/dldr/deluge_config.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -e
 
 /home/nobody/config_deluge.py "/config/core.conf" "plugins_location" "/plugins"
 /config_deluge_type.py "/config/core.conf" "enabled_plugins" "Pieces" "array"
-
-exec /usr/local/bin/init.sh
