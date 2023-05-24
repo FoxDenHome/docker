21b292faf06fe5febef5a11dd2077182bb289260
Doridian <git@doridian.net>
2023-05-24 00:53:29 -0700

    deluge dark theme

--- nas/dldr/deluge_config.sh
+++ nas/dldr/deluge_config.sh
@@ -2,3 +2,5 @@
 
 /home/nobody/config_deluge.py "/config/core.conf" "plugins_location" "/plugins"
 /config_deluge_type.py "/config/core.conf" "enabled_plugins" "Pieces" "array"
+
+/home/nobody/config_deluge.py "/config/web.conf" "theme" "dark"
