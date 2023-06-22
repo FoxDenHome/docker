d441004a7a0bb97b422eb795a00247d75df57d80
Doridian <git@doridian.net>
2023-06-21 21:36:13 -0700

    SRIOV installer

---
+++ _utils/sriov/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+VERSION="1.5.0"
+
+rm -f /usr/local/bin/docker-sriov-plugin /etc/systemd/system/docker-sriov-plugin.service /tmp/sriov-plugin.tar.gz
+wget "https://github.com/FoxDenHome/docker-sriov-plugin/releases/download/$VERSION/docker-sriov-plugin-linux-amd64.tar.gz" -O /tmp/sriov-plugin.tar.gz
+tar -C /usr/local/bin -xvf /tmp/sriov-plugin.tar.gz
+mv /usr/local/bin/docker-sriov-plugin.service /etc/systemd/system/docker-sriov-plugin.service
+chmod +x /usr/local/bin/docker-sriov-plugin
+
+systemctl daemon-reload
+systemctl enable docker-sriov-plugin
+systemctl restart docker-sriov-plugin
