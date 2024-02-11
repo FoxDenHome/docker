25b4efc5256ea3ef70b4a14812499555ba7c2856
Doridian <git@doridian.net>
2024-02-11 10:43:09 -0800

    Use Caddyfile env vars in syncthing

--- syncthing/webdav/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-sed "s~__SYNCTHING_DOMAIN__~${SYNCTHING_DOMAIN}~g" /etc/caddy/Caddyfile.tpl > /etc/caddy/Caddyfile
-
-exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
