e37b0692b6369bba9bad706bc353bdcbac193b28
Doridian <git@doridian.net>
2024-07-17 17:13:54 -0700

    try

--- xmpp/prosody/conf/conf.d/90-slidge.cfg.lua
+++ xmpp/prosody/conf/conf.d/90-slidge.cfg.lua
@@ -4,9 +4,9 @@ component_interfaces = { "0.0.0.0" }
 Component "discord.xmpp.foxden.network"
   component_secret = os.getenv("SLIDCORD_SECRET")
   modules_enabled = {"isolate_host"}
-  isolate_except_domains = {"foxden.network", "xmpp.foxden.network"}
+  isolate_except_domains = {"foxden.network", "xmpp.foxden.network", "upload.xmpp.foxden.network"}
 
 Component "telegram.xmpp.foxden.network"
   component_secret = os.getenv("SLIDGRAM_SECRET")
   modules_enabled = {"isolate_host"}
-  isolate_except_domains = {"foxden.network", "xmpp.foxden.network"}
+  isolate_except_domains = {"foxden.network", "xmpp.foxden.network", "upload.xmpp.foxden.network"}
