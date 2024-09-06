2a3cb91f6d9cba085696545b6215ef74a78245b9
Doridian <git@doridian.net>
2024-09-06 14:04:44 -0700

    rmrf authentik

--- authentik/certbot-cron/custom-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# certbot certonly --non-interactive --agree-tos -m ssl@foxden.network -d auth.foxden.network --standalone
-
-while :; do
-    certbot renew --non-interactive
-    chown -R 1000:1000 /etc/letsencrypt
-    sleep 24h
-done
