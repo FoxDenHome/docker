e47497bfcb387b72a0d0a13a3c09178f5f7830f6
Doridian <git@doridian.net>
2025-01-06 16:36:20 -0800

    redir

--- minecraft/server/entrypoint.sh
+++ minecraft/server/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+set -ex
+rm -f /tmp/minecraft-fifo
 mkfifo /tmp/minecraft-fifo
 cat /tmp/minecraft-fifo | java -Xmx8G -Xms8G -Djline.terminal=jline.UnsupportedTerminal -jar server.jar nogui
